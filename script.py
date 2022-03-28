81cc233341723ce31040efca66f275fea71d7bff
jfaro <jfaro@umich.edu>
2022-03-28 17:58:10 -0400

    Update workload generation script

--- script.py
+++ script.py
@@ -1,55 +1,54 @@
 import os
 import subprocess
+import json
 from time import sleep
+from wrk_parser import parse_wrk_output
 
-
-# Configuration
 PATH_TO_SOCIAL_NETWORK = '/home/jfaro/src/DeathStarBench/socialNetwork'
 WRK_DIR = 'wrk2'
 
+# Run workload
+NUM_THREADS = 1
+NUM_CONNECTIONS = 100
+DURATION = 10
+REQUESTS_PER_SECOND = 50
+SCRIPT_TO_RUN = './scripts/social-network/compose-post.lua'
+URL = 'http://localhost:8080/wrk2-api/post/compose'
 
-# Process management
-# TODO: append new processes here
-processes = [
-    # workload
-    # start/stop network connection
-]
+
+def cmd_output(command_list):
+    process = subprocess.run(command_list, stdout=subprocess.PIPE, universal_newlines=True)
+    return process.stdout
 
 
-def main():
+# Generate workload with wrk
+def run_wrk():
+
     # Change to wrk directory
     wrk_dir = os.path.join(PATH_TO_SOCIAL_NETWORK, WRK_DIR)
     os.chdir(wrk_dir)
 
-    # Run workload
-    NUM_THREADS = 1
-    NUM_CONNECTIONS = 1000
-    DURATION = 10
-    REQUESTS_PER_SECOND = 50
-    SCRIPT_TO_RUN = './scripts/social-network/compose-post.lua'
+    print("Active directory:", cmd_output(['pwd']))
 
-    command = ['./wrk', '-D', 'exp',
-        '-t', str(NUM_THREADS),
-        '-c', str(NUM_CONNECTIONS),
-        '-d', str(DURATION),
-        '-L', '-s',
-        SCRIPT_TO_RUN, 
-        'http://localhost:8080/wrk2-api/post/compose',
-        '-R', str(REQUESTS_PER_SECOND)
+    # wrk2 command
+    args = [
+        './wrk', 
+        '-D', 'exp',                    # fixed, exp, norm, zipf
+        f'-t {NUM_THREADS}',            # Number of threads to use
+        f'-c {NUM_CONNECTIONS}',        # Connections to keep open
+        f'-d {DURATION}s',              # Duration to test
+        '--latency',                    # Output latency information
+        f'-R {REQUESTS_PER_SECOND}',    # Work rate (throughput) in requests/sec (total)
+
+        f'-s {SCRIPT_TO_RUN}',
+        URL,
     ]
 
     print("Generating workload...")
-    process = subprocess.run(command, stdout=subprocess.PIPE, universal_newlines=True)
-    sleep(.5)
-
-    print("Disconnecting from network")
-    change_network_connection('disconnect')
-    sleep(5)
-
-    print("Reconnecting network")
-    change_network_connection('connect')
-
-    print(process.stdout)
+    print(" ".join(args))
+    args_str = " ".join(args)
+    process = subprocess.Popen(args_str, stdout=subprocess.PIPE, text=True, shell=True)
+    return process
 
 
 # Action: 'disconnect' | 'connect'
@@ -65,4 +64,23 @@ def change_network_connection(action='disconnect'):
     subprocess.run(command, stdout=subprocess.PIPE, universal_newlines=True)
 
 
-main()
+def main():
+
+    # Create workload
+    wrk_process = run_wrk()
+
+    # Wait for process to complete
+    while wrk_process.poll() is None:
+        print("wrk running...")
+        sleep(2)
+
+    # Get output
+    stdout, stderr = wrk_process.communicate()
+    print(stdout)
+
+    wrk_output_dict = parse_wrk_output(stdout)
+    print(json.dumps(wrk_output_dict, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()
